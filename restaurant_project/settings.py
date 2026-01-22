448b63887cec4a5e977147a8b2588fd788f20a85
vadimzhenchur <141917559+vadimzhenchur@users.noreply.github.com>
2026-01-22 18:36:35 +0200

    156

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -72,7 +72,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = Path(BASE_DIR) / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles"]
+    BASE_DIR / "static"]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
