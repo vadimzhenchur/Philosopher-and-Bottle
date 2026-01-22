1a94b1c1839c3ac0a386553345c15e19a94fd990
vadimzhenchur <141917559+vadimzhenchur@users.noreply.github.com>
2026-01-22 17:04:42 +0200

    fin

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -69,11 +69,11 @@ TIME_ZONE = 'Europe/Kyiv'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = Path(BASE_DIR) / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "static"]
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 CLOUDINARY_STORAGE = {
